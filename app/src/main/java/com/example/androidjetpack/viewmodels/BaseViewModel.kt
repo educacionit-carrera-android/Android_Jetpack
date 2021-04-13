9932efa3095b68c95dbcf873fdc8ee8e1d418cf0
Nicolasd <nicolas@thelucky.app>
2021-04-13 00:25:26 -0300

    Agregar LiveData mensaje en BaseViewModel

--- app/src/main/java/com/example/androidjetpack/viewmodels/BaseViewModel.kt
+++ app/src/main/java/com/example/androidjetpack/viewmodels/BaseViewModel.kt
@@ -4,7 +4,10 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.ViewModel
 import com.example.androidjetpack.helpers.SingleLiveEvent
 
-class BaseViewModel : ViewModel() {
-    protected val showLoading = SingleLiveEvent<Boolean>()
-    val getShowLoading: LiveData<Boolean> = showLoading
+open class BaseViewModel : ViewModel() {
+    protected val _showLoading = SingleLiveEvent<Boolean>()
+    val showLoading: LiveData<Boolean> = _showLoading
+
+    protected val _mensaje = SingleLiveEvent<String>()
+    val mensaje: LiveData<String> = _mensaje
 }
